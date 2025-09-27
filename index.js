9e118d593d1affa73c6c786369aea4ca2ca51bc5
harshitdynamite@gmail.com <harshitdynamite@gmail.com>
2025-09-27 07:29:14 +0400

    Simple SSE code

---
+++ index.js
@@ -0,0 +1,27 @@
+const app = require("express")();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/stream", (req, res) => {
+  res.setHeader("Content-Type", "text/event-stream");
+  send(res);
+
+  });
+
+  let i=0;
+  function send(res)
+  {
+
+    res.write("data: " + "hello" + i + "\n\n");
+
+    i++;
+
+    setTimeout(() =>
+      send(res)
+    ,1000);
+  }
+
+app.listen(8080);
+console.log("Server is listening on http://localhost:8080");
